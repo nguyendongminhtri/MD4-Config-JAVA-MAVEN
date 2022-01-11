11997eab0abe0dcf3d42d121736f6a745a81b935
nguyendongminhtri <73800938+nguyendongminhtri@users.noreply.github.com>
2022-01-11 09:52:29 +0700

    MAVEN

---
+++ src/main/java/chinh/anh/config/WebConfig.java
@@ -0,0 +1,21 @@
+package chinh.anh.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("chinh.anh.controller")
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    InternalResourceViewResolver resolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
